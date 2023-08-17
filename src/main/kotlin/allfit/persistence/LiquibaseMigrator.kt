8380b01be5cfb4bd8f48bab060242d9858c24400
Christoph Pickl <christophpickl@users.noreply.github.com>
2023-08-17 10:11:56 +0200

    use new way of liquibase migration

--- src/main/kotlin/allfit/persistence/LiquibaseMigrator.kt
+++ src/main/kotlin/allfit/persistence/LiquibaseMigrator.kt
@@ -2,10 +2,11 @@ package allfit.persistence
 
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import java.sql.DriverManager
-import liquibase.Liquibase
+import liquibase.command.CommandScope
+import liquibase.command.core.UpdateCommandStep
+import liquibase.command.core.helpers.DbUrlConnectionCommandStep
 import liquibase.database.DatabaseFactory
 import liquibase.database.jvm.JdbcConnection
-import liquibase.resource.ClassLoaderResourceAccessor
 
 object LiquibaseMigrator {
 
@@ -16,8 +17,11 @@ object LiquibaseMigrator {
         log.info { "Migrating database..." }
         val connection = DriverManager.getConnection(config.jdbcUrl, config.username, config.password)
         val database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(connection))
-        val liquibase = Liquibase(CHANGELOG_CLASSPATH, ClassLoaderResourceAccessor(), database)
-        liquibase.update()
+        val updateCommand = CommandScope(*UpdateCommandStep.COMMAND_NAME)
+        updateCommand.addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, database);
+        updateCommand.addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, CHANGELOG_CLASSPATH);
+        updateCommand.execute()
+
         log.info { "Migrating database done ✅" }
     }
 }
