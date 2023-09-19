c070e816dd748af411e2ba1325c0aa8c6672f4c6
Christoph Pickl <christophpickl@users.noreply.github.com>
2023-09-19 16:21:53 +0200

    fix compile error in test source

--- src/test/kotlin/allfit/sync/domain/InMemoryWorkoutFetcher.kt
+++ src/test/kotlin/allfit/sync/domain/InMemoryWorkoutFetcher.kt
@@ -4,6 +4,6 @@ class InMemoryWorkoutFetcher : WorkoutMetadataFetcher {
 
     val urlToFetches = mutableMapOf<WorkoutUrl, WorkoutFetchMetadata>()
 
-    override suspend fun fetch(url: WorkoutUrl) =
+    override suspend fun fetch(url: WorkoutUrl, listener: WorkoutMetadataFetchListener) =
         urlToFetches[url] ?: error("Fetch result not found for: $url")
 }
